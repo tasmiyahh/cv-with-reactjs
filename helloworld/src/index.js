de63952fa677e504af7031c3d2349a6e5480c906
tasmiyahh <tasmiyahhussainn99@gmail.com>
2022-09-05 16:18:42 +0500

    new

---
+++ helloworld/src/index.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css'
+
+
+function Hi() {
+  return <div className='body'>
+   
+<div className='main'>
+
+   <p>
+    <h1 >
+      TASMIYAH HUSSAIN </h1>
+
+    <p className='header'><b>Mobile</b>: 03070004567.</p>
+   <p className='header'>
+   <b>Address</b>: fb area block 13.
+   </p>
+   <p className='header'>
+   <b>Email</b>: tasmiyahhussainn99@gmail.com.
+   </p>
+
+   </p>
+
+
+
+   <p>
+    <h2>
+
+      OBJECTIVE
+    </h2>
+    <p>
+      To excel among the Professionals. Where I can utitlize my skills with strong commitment and dedication, for the
+      progressive growth of the company.
+    </p>
+</p>
+
+
+<p>
+
+    <h2>
+
+      EDUCATION
+
+    </h2>
+
+    <ul>
+      <li>DECEMBER,21
+        Bachelors of Commerce. University Of Karachi.  </li>
+      <li> JULY,18
+        Intermediate (metropolis college)  </li>
+      <li>JULY,16
+        Matriculation(Rose Petal School) </li>
+    </ul>
+</p>
+
+
+
+
+
+
+
+<p>
+    <h2>
+
+      HOBBIES</h2>
+
+    -  Reading
+    -Music
+    -writing
+
+</p>
+
+
+ <p>
+ 
+    <h2>
+
+      SKILLS
+
+    </h2>
+
+    <ul>
+      <li>Html</li>
+      <li>Css</li>
+    </ul>
+</p>
+
+
+
+<p>
+    <h2>
+
+      LANGUAGES
+    </h2>
+    <ul>
+      <li>English</li>
+      <li>Urdu</li>
+    </ul>
+</p>
+
+<p>
+    <h2>
+
+      REFERENCE
+
+    </h2>
+    <p>
+      will be furnished on request
+    </p>
+</p>
+
+
+
+
+</div>
+
+
+
+
+  </div>
+}
+
+
+
+
+ReactDOM.render(<Hi />, document.querySelector('#root'));
